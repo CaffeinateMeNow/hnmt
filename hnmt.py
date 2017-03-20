198a6c26a8fdbf76c24d0471dfbfa96d9f31c76f
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2017-03-20 15:47:43 +0200

    off-by-one in unk indexing

--- hnmt.py
+++ hnmt.py
@@ -650,9 +650,9 @@ class NMT(Model):
         # char level decoder
         embedded_char_outputs = self.trg_char_embeddings(out_chars)
         char_h_0 = self.proj_char_h0(
-            h_seq[charlevel_indices[0], charlevel_indices[1], :])
+            h_seq[charlevel_indices[0] - 1, charlevel_indices[1], :])
         char_c_0 = self.proj_char_c0(
-            c_seq[charlevel_indices[0], charlevel_indices[1], :])
+            c_seq[charlevel_indices[0] - 1, charlevel_indices[1], :])
         char_h_seq, char_states, char_outputs = self.char_decoder(
                 embedded_char_outputs, out_chars_mask,
                 [char_h_0, char_c_0])
